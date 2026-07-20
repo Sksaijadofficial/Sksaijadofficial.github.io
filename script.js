340ecb32fc10b5a53a7db4344c9a7cf7006c423f
Sk Saijad <saijadsk414@gmail.com>
2026-07-20 15:01:32 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+// Sk Saijad Official Website
+
+console.log("Welcome to Sk Saijad Official");
+
+window.onload = function () {
+    document.body.style.scrollBehavior = "smooth";
+};
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", function(e){
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if(target){
+            target.scrollIntoView({
+                behavior:"smooth"
+            });
+        }
+    });
+});
+
+// Welcome Message
+setTimeout(function(){
+    console.log("Website Loaded Successfully");
+},1000);
