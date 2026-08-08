070223a3cbfd2a9875435889442393e05efa0396
Sk Saijad <saijadsk414@gmail.com>
2026-08-08 19:43:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -62,3 +62,22 @@ function topFunction() {
         behavior: "smooth"
     });
 }
+// 🌙 Dark Mode
+const themeBtn = document.getElementById("themeBtn");
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+    themeBtn.textContent = "☀️";
+}
+
+themeBtn.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeBtn.textContent = "☀️";
+        localStorage.setItem("theme", "dark");
+    } else {
+        themeBtn.textContent = "🌙";
+        localStorage.setItem("theme", "light");
+    }
+});
